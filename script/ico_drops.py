4ecc445beff5150e6551be4858a753bb155b3b02
Marine Bauerlé <145704547+MarineBauerle@users.noreply.github.com>
2023-11-22 11:10:18 +0000

    Add files via upload
    Here's a summary of the steps taken according to the Python script:
    
    Import Libraries: Pandas, for data manipulation, and pycountry, for country code conversion, are imported.
    
    Function Definition: A function named get_country_name is defined to convert two-letter country codes (alpha-2) into full country names using the pycountry library. If a conversion fails, it returns None.
    
    Read the CSV File: The original CSV file is loaded into a pandas DataFrame with semicolon (;) as the delimiter.
    
    Column Cleanup: Any potential leading or trailing spaces in the column names are removed to avoid key errors when referencing column names.
    
    Apply the Function: The get_country_name function is applied to the 'Country' column to replace the two-letter country codes with the full country names.
    
    Save the Updated DataFrame: Finally, the DataFrame with updated country names is saved to a new CSV file, preserving the changes without overwriting the original data.
    
    This process enhances the readability of the data and facilitates better understanding and analysis by replacing less informative country codes with full country names.

--- script/ico_drops.py
+++ script/ico_drops.py
@@ -20,11 +20,11 @@ print(df.columns)
 # If 'Country' is indeed the column name, strip any potential leading/trailing spaces from column names
 df.columns = df.columns.str.strip()
 
-# Apply the function to the 'Country' column with the correct case
+# Now apply the function to the 'Country' column with the correct case
 df['Country'] = df['Country'].apply(get_country_name)
 
 # Print the first few rows to verify the country names have been updated
 print(df.head())
 
-# Save the updated dataframe to a new CSV file
+# Optionally, save the updated dataframe to a new CSV file
 df.to_csv('Updated ICO drops - referring domains.csv', index=False)
